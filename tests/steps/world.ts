2870a6b82be5a5b3daa63412f9b2b459b5960806
Dawson <101001810+Dawsoncodes@users.noreply.github.com>
2024-03-28 05:08:21 +0300

    lt integrated

--- tests/steps/world.ts
+++ tests/steps/world.ts
@@ -1,3 +1,5 @@
+import { config } from "dotenv"
+config()
 import { After, Before, setDefaultTimeout } from "@cucumber/cucumber"
 import { Browser, Page, chromium } from "@playwright/test"
 
@@ -6,8 +8,28 @@ let browser: Browser
 
 setDefaultTimeout(60000)
 
-Before(async () => {
-  browser = await chromium.launch()
+Before(async ({ pickle }) => {
+  const capabilities = {
+    browserName: "Chrome", // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    browserVersion: "latest",
+    "LT:Options": {
+      platform: "Windows 10",
+      build: "Playwright Cucumber Tutorial",
+      name: pickle.name,
+      user: process.env.LT_USERNAME,
+      accessKey: process.env.LT_ACCESS_KEY,
+      network: true,
+      video: true,
+      console: true,
+    },
+  }
+
+  browser = await chromium.connect({
+    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
+      JSON.stringify(capabilities)
+    )}`,
+  })
+
   page = await browser.newPage()
 })
 
